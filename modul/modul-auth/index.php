27e766f264d834f0408a4c196f44cf7515be9ad5
Abyravka90 <asepcahya.nugraha75@gmail.com>
2023-02-09 10:33:36 +0700

    rpl 1 auth

---
+++ modul/modul-auth/index.php
@@ -0,0 +1,88 @@
+<?php
+include('../../config/database.php');
+if (isset($_POST['cek'])) {
+    $pilihan = $_POST['pilihan'];
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    if ($pilihan == 'masyarakat') {
+        $q = mysqli_query($con, "SELECT * FROM `masyarakat` WHERE username = '$username' AND password = '$password'");
+        $r = mysqli_num_rows($q);
+        if ($r == 1) {
+            $d = mysqli_fetch_object($q);
+            @session_start();
+            $_SESSION['username'] = $d->username;
+            $_SESSION['level'] = 'masyarakat';
+            @header('location:../../modul/modul-masyarakat/');
+        }
+    } else if ($pilihan == 'petugas') {
+    }
+
+    // echo $username;
+    // echo $password;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- header -->
+<?php include('../../assets/header.php') ?>
+
+<body">
+    <div class="wrapper">
+        <!-- Content Wrapper. Contains page content -->
+        <!-- Main content -->
+        <section class="content ">
+            <div class="container-fluid">
+                <div class="row justify-content-md-center">
+                    <div class="col-md-3" style="margin-top:10%">
+                        <!-- jquery validation -->
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h3 class="card-title"><i class="fa fa-users">&nbsp;</i>Login <small>SISPENMAS</small></h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <!-- form start -->
+                            <form action="" method="post">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Username</label>
+                                        <input type="text" name="username" class="form-control" placeholder="Masukan Username">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input type="password" name="password" class="form-control" placeholder="Masukan Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <select class="form-control" name="pilihan">
+                                            <option value="masyarakat">masyarakat</option>
+                                            <option value="petugas">Petugas</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group mb-0">
+                                        <span class="text text-success">Belum terdaftar?</span>Silahkan daftar <a href="">disini</a>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="form-group">
+                                        <button name="cek" type="submit" class="form-control btn btn-primary">Masuk</button>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+                            </form>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.container-fluid -->
+        <!-- /.content -->
+        <!-- /.content-wrapper -->
+    </div>
+    <!-- ./wrapper -->
+    <?php include('../../assets/footer.php') ?>
+
+    </body>
+
+</html>
